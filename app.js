c01fe09adf8dbc461b60f796ebbbb56952bb38cf
Sarthak1918 <pandasarthak12345@gmail.com>
2023-10-02 13:40:39 +0530

    update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: "https://sarthak-mern-todo.netlify.app",
     methods: ["GET", "PUT", "POST", "DELETE"],
     credentials: true,
 }))
